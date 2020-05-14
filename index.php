bad4542fe99c18206bf118eb333d010817379a09
jerryhopper <hopper.jerry@gmail.com>
2020-05-14 15:13:07 +0200

    ..

--- index.php
+++ index.php
@@ -10,7 +10,12 @@ require 'src/blackbox/bbExec.php';
 require 'src/blackbox/bbDatabase.php';
 require 'src/OsboxDatabase/Users.php';
 
-require 'src/PiholeNativeAuth.php';
+
+require 'src/PiHole/PiholeNativeAuth.php';
+
+require 'src/PiHole/phDatabase.php';
+require 'src/PiHole/Database/Client.php';
+require 'src/PiHole/Database/ClientByGroup.php';
 
 require 'src/blackbox/OauthUserObj.php';
 require 'src/blackbox/bbInit.php';
@@ -34,7 +39,7 @@ require 'src/SQLite3.php';
 
 */
 
-
+session_start();
 // Create Slim app
 $app = new \Slim\App();
 
@@ -53,7 +58,6 @@ $container['cookie'] = function($c){
 $container['BlackBox'] = function($c){
     return new BlackBox();
 };
-
 
 // Register Twig View helper
 $container['view'] = function ($c) {
@@ -77,7 +81,7 @@ $app->add(function ($request, $response, $next) {
 
     //$response->getBody()->write('BEFORE');
 
-    if ( !is_null( $this->cookie->get("auth") ) ){
+    if ( !is_null( $this->cookie->get("auth") ) OR  $this->cookie->get("auth")!="" ){
         $this->BlackBox->validateToken( $this->cookie->get("auth") );
         #var_dump($this->cookie->get("auth") );
         #print_r($this->cookie->get("persistentlogin"));
@@ -93,6 +97,11 @@ $app->add(function ($request, $response, $next) {
 });
 
 
+// Generate CSRF token
+if(empty($_SESSION['token'])) {
+    $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+}
+$token = $_SESSION['token'];
 
 
 /**
@@ -119,10 +128,23 @@ $app->get('/test', function ($request, $response, $args) {
 })->setName('page_dashboard');
 
 
+$app->get('/devices', function ($request, $response, $args) {
+    return $this->view->render( $response, $this->BlackBox->showpage( "devices.html", $request ), $this->BlackBox->UiParameters(["CSRFTOKEN"=>$_SESSION['token'],"PAGE"=>".page_uwapparaten"]));
+})->setName('page_uwapparaten');
+
+$app->get('/users', function ($request, $response, $args) {
+    return $this->view->render( $response, $this->BlackBox->showpage( "users.html", $request ), $this->BlackBox->UiParameters(["CSRFTOKEN"=>$_SESSION['token'],"PAGE"=>".page_users"]));
+})->setName('page_users');
+
+$app->get('/groups', function ($request, $response, $args) {
+    return $this->view->render( $response, $this->BlackBox->showpage( "groups.html", $request ), $this->BlackBox->UiParameters(["CSRFTOKEN"=>$_SESSION['token'],"PAGE"=>".page_groups"]));
+})->setName('page_groups');
+
+
+
 
 // Define home route
 $app->get('/', function ($request, $response, $args) {
-
 //    return $this->view->render($response,"register/index.html");
     #[authenticated] => 1
     #[user][userId] => 6ab331fb-e654-4de3-aa29-b403fcd557e1
@@ -130,14 +152,81 @@ $app->get('/', function ($request, $response, $args) {
     #print_r($request->getAttribute("AUTH"));
     #die();
 
-
     #die();
 
     //return $this->view->render( $response, "default/dashboard.html", $this->BlackBox->UiParameters(["PAGE"=>".page_dashboard"]));
     //return $response->withJson("whooo");
-    return $this->view->render( $response, $this->BlackBox->showpage( "default/dashboard.html", $request ), $this->BlackBox->UiParameters(["PAGE"=>".page_dashboard"]));
+    return $this->view->render( $response, $this->BlackBox->showpage( "default/dashboard.html", $request ), $this->BlackBox->UiParameters(["CSRFTOKEN"=>$_SESSION['token'],"PAGE"=>".page_dashboard"]));
 })->setName('page_dashboard');
 
+
+$app->get('/api/users', function ($request, $response, $args) {
+
+    return $response->withJson([]);
+});
+
+$app->get('/api/move/{ip}/togroup/{piholegroup}', function ($request, $response, $args) {
+
+
+    $done = $this->BlackBox->PiHole->moveIpToGroup( $args['ip'], $args['piholegroup'] );
+
+    //$done = array("IP"=>$args['ip'],"GROUP"=>$args['piholegroup']);
+
+    return $response->withJson($done);
+});
+
+
+/*******************************************************************************************
+ *
+ *  ..
+ *
+ *******************************************************************************************/
+
+
+$app->get('/login', function ($request, $response, $args) {
+    return $response->withRedirect( $this->BlackBox->getLoginUrl() );
+});
+
+$app->get('/logout', function ($request, $response, $args) {
+    // set the cookie
+    $setcookies = new Slim\Http\Cookies();
+    $setcookies->set('auth',[
+        'value' => "",
+        'expires' => "yesterday",
+        'path' => '/',
+        'domain' => 'blackbox.surfwijzer.nl',
+        'httponly' => true,
+        'hostonly' => false,
+        'secure' => true,
+        'samesite' => 'lax'
+    ]);
+    $setcookies->set('persistentlogin',[
+        'value' => "",
+        'expires' => "yesterday",
+        'path' => '/admin',
+        'domain' => 'blackbox.surfwijzer.nl',
+        'httponly' => true,
+        'hostonly' => false,
+        'secure' => true,
+        'samesite' => 'lax'
+    ]);
+    /*
+     *
+     $setcookies->set('persistentlogin',[
+        'value' => "",
+        'expires' => "yesterday",
+        'path' => '/',
+        'domain' => 'blackbox.surfwijzer.nl',
+        'httponly' => true,
+        'hostonly' => false,
+        'secure' => true,
+        'samesite' => 'lax'
+    ]);
+     */
+    $response = $response->withHeader('Set-Cookie', $setcookies->toHeaders());
+
+    return $response->withRedirect( '/');
+})->setName('page_logout');
 
 /**
  *  Callback function.
@@ -211,10 +300,11 @@ $app->get('/callback', function ($request, $response, $args) {
             'secure' => true,
             'samesite' => 'lax'
         ]);
+
         $setcookies->set('persistentlogin',[
             'value' => $this->BlackBox->setupVars["WEBPASSWORD"],
             'expires' => $this->BlackBox->getUserinfo('expires'),
-            'path' => '/',
+            'path' => '/admin',
             'domain' => 'blackbox.surfwijzer.nl',
             'httponly' => true,
             'hostonly' => false,
@@ -227,13 +317,6 @@ $app->get('/callback', function ($request, $response, $args) {
 
     }
 });
-
-
-/*******************************************************************************************
- *
- *  ..
- *
- *******************************************************************************************/
 
 
 
